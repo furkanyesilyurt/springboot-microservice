9bec3e14aa50a71569207b9023d84db33131ddb5
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2023-01-12 01:56:40 +0300

    initial

--- book-service/src/main/java/com/bookshelf/bookservice/dto/BookDto.java
+++ book-service/src/main/java/com/bookshelf/bookservice/dto/BookDto.java
@@ -1,4 +1,9 @@
 package com.bookshelf.bookservice.dto;
 
 public class BookDto {
+    private Long id;
+    private String title;
+    private String bookYear;
+    private String author;
+    private String pressName;
 }
